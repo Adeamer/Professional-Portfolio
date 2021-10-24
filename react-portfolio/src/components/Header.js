e8cefe03c72925fca1c69e4095d82fe23dafe300
Elziekio <anthony.deamer@hotmail.com>
2021-10-24 11:54:07 +1000

    Adding CSS file and amended the src path for image

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import Navigation from './Navigation';
 import Project from './Project';
 import Footer from './Footer';
+import '../Styles/Header.css';
 
 function Header() {
 
     return (
         <div>
             <header class="header">
-                <img id="Profile-Pic" src="./Images/Profile Pic.jpg" alt="Profile Picture"/>
+                <img id="Profile-Pic" src="../Images/Profile Pic.jpg" alt="Profile Picture"/>
                 <h1 id="name">Anthony Deamer</h1>
 
             </header>
