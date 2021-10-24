f0af4413aadd2bc35a0579d336ac48f4c76d668a
Elziekio <anthony.deamer@hotmail.com>
2021-10-24 12:59:06 +1000

    Added function for about me JSX

---
+++ react-portfolio/src/components/AboutMe.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import '../Styles/About-me.css';
+
+function AboutMe() {
+
+    return (
+        <div>
+            <h1 id="about-me">About Me</h1>
+            <article id="about-me1">
+                <p>I am fairly new to software and web development, I have spent the last 7 years in the logistics and trade industry. Throughout that time I gained a lot of experience. Not only with computers and navigating programs, but also creating and maintaining strong relationships with cliental and team members. I am now looking to expand what I have learned over this time and incorporate it into future positions I may get in the coding world.</p>
+            </article>
+        </div>
+    );
+}
+
+export default AboutMe;
