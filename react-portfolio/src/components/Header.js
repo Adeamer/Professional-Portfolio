12af2a412751eeff473f3550e64fa41eb4a2d146
Elziekio <anthony.deamer@hotmail.com>
2021-10-24 12:22:48 +1000

    Added navigation JSX and updated image for react

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -3,15 +3,16 @@ import Navigation from './Navigation';
 import Project from './Project';
 import Footer from './Footer';
 import '../Styles/Header.css';
+import ProfilePic from '../Images/Profile-Pic.jpg';
 
 function Header() {
 
     return (
         <div>
             <header class="header">
-                <img id="Profile-Pic" src="../Images/Profile-Pic.jpg" alt="Profile Picture"/>
+                <img id="Profile-Pic" src={ProfilePic} alt="Profile Picture"/>
                 <h1 id="name">Anthony Deamer</h1>
-
+                <Navigation/>
             </header>
         </div>
     );
