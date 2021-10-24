8ed61213598e9024781f1bb2ca56be314b2a3a31
Elziekio <anthony.deamer@hotmail.com>
2021-10-24 11:58:57 +1000

    Updated src path for profile pic

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
     return (
         <div>
             <header class="header">
-                <img id="Profile-Pic" src="../Images/Profile Pic.jpg" alt="Profile Picture"/>
+                <img id="Profile-Pic" src="../Images/Profile-Pic.jpg" alt="Profile Picture"/>
                 <h1 id="name">Anthony Deamer</h1>
 
             </header>
