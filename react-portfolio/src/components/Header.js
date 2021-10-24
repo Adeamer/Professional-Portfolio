c0ff6541529cf6978de26180e8255e4e2e63544a
Elziekio <anthony.deamer@hotmail.com>
2021-10-24 11:42:51 +1000

    Update header componement with basic HTML elements

---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Navigation from './Navigation';
+import Project from './Project';
+import Footer from './Footer';
+
+function Header() {
+
+    return (
+        <div>
+            <header class="header">
+                <img id="Profile-Pic" src="./Images/Profile Pic.jpg" alt="Profile Picture"/>
+                <h1 id="name">Anthony Deamer</h1>
+
+            </header>
+        </div>
+    );
+}
+
+export default Header;
