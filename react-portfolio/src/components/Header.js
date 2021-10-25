c01619736f7c0f6de6811bcb46c5e80c56a02162
Elziekio <anthony.deamer@hotmail.com>
2021-10-25 12:43:17 +1000

    Adding logic for rendering page via navbar

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -7,15 +7,30 @@ import '../Styles/Header.css';
 import ProfilePic from '../Images/Profile-Pic.jpg';
 
 function Header() {
+    const [currentPage, setCurrentPage] = useState('About-me');
+
+    const renderPage = () => {
+        if (currentPage === 'AboutMe') {
+            return <AboutMe/>;
+        }
+        if (currentPage === 'Project') {
+            return <Project/>;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume/>;
+        }
+    }
+
+    const handlePageChange = (page) => setCurrentPage(page);
 
     return (
         <div>
             <header class="header">
                 <img id="Profile-Pic" src={ProfilePic} alt="Profile Picture"/>
                 <h1 id="name">Anthony Deamer</h1>
-                <Navigation/>
+                <Navigation currentPage={currentPage} handlePageChange={handlePageChange}/>
             </header>
-            <AboutMe/>
+            {renderPage()}
         </div>
     );
 }
