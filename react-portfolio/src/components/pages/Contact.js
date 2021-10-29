6c90d06316becec71097ae0fb62c8cc008f56bf4
Elziekio <anthony.deamer@hotmail.com>
2021-10-29 12:23:55 +1000

    Adding function for handling form

--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -1,28 +1,48 @@
 import React, { useState } from "react";
-import '../../Styles/Contact.css';
+// import '../../Styles/Contact.css';
 
 
 function Contact() {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: 'Enter email address'
+        };
 
-    return (
-        <div>
-            <form>
-                <div class="form-floating mb-3">
-                    <textarea class="form-control" id="floatingInput" placeholder="name@example.com"></textarea>
-                    <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating">
-                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-                    <label for="floatingTextarea">Name</label>
-                </div>
-                <div class="form-floating">
-                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-                    <label for="floatingTextarea2">Comments</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    );
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ calue: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('Your message has been submitted and will be responded to shortly.');
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-floating mb-3">
+                        <textarea value={this.state.value} onChange={this.handleChange} className="form-control" id="floatingInput" placeholder="name@example.com"></textarea>
+                        <label for="floatingInput">Email address</label>
+                    </div>
+                    <div class="form-floating">
+                        <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                        <label for="floatingTextarea">Name</label>
+                    </div>
+                    <div className="form-floating">
+                        <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                        <label for="floatingTextarea2">Comments</label>
+                    </div>
+                    <button type="submit" value='Submit' className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Contact;
