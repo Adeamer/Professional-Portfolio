b78b50d23cf3db11522ec11d48db68161c0cab80
Elziekio <anthony.deamer@hotmail.com>
2021-10-30 09:53:03 +1000

    Amended contact form to to render page

--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
-// import '../../Styles/Contact.css';
+import '../../Styles/Contact.css';
 
 
 function Contact() {
 
-
+// Set inital state
     return (
         <div>
+            <h2 className='contact-title'>Contact Me</h2>
             <form>
                 <div className="form-floating mb-3">
                     <textarea className="form-control" id="floatingInput" placeholder="name@example.com"></textarea>
-                    <label for="floatingInput">Email address</label>
+                    <label htmlFor="floatingInput">Email address</label>
                 </div>
                 <div class="form-floating">
                     <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-                    <label for="floatingTextarea">Name</label>
+                    <label htmlFor="floatingTextarea">Name</label>
                 </div>
+                <br></br>
                 <div className="form-floating">
-                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-                    <label for="floatingTextarea2">Comments</label>
+                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: "100px"}}></textarea>
+                    <label htmlFor="floatingTextarea2">Message</label>
                 </div>
+                <br></br>
                 <button type="submit" value='Submit' className="btn btn-primary">Submit</button>
             </form>
         </div>
