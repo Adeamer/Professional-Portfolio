f1cbeab16709ceb536c3a6147b5f1241ae6ab919
Elziekio <anthony.deamer@hotmail.com>
2021-10-30 10:28:18 +1000

    Adding functions for validating email and setting errors

--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -1,27 +1,64 @@
 import React, { useState } from "react";
 import '../../Styles/Contact.css';
+import { validateEmail } from '../../utils/helpers';
 
 
 function Contact() {
 
-// Set inital state
+    const [formState, setFormState] = useState({ email: '', name: '', message: '' });
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const { email, name, message } = formState;
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+
+            if (!isValid) {
+                setErrorMessage('Please enter valid email');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value })
+        }
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
         <div>
             <h2 className='contact-title'>Contact Me</h2>
             <form>
                 <div className="form-floating mb-3">
-                    <textarea className="form-control" id="floatingInput" placeholder="name@example.com"></textarea>
+                    <textarea className="form-control" id="floatingInput" defaultValue={email} onChange={handleChange} ></textarea>
                     <label htmlFor="floatingInput">Email address</label>
                 </div>
                 <div class="form-floating">
-                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                    <textarea className="form-control" id="floatingTextarea" defaultValue={name} onChange={handleChange} ></textarea>
                     <label htmlFor="floatingTextarea">Name</label>
                 </div>
                 <br></br>
                 <div className="form-floating">
-                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: "100px"}}></textarea>
+                    <textarea className="form-control" id="floatingTextarea2" style={{ height: "100px" }} defaultValue={message} onChange={handleChange} ></textarea>
                     <label htmlFor="floatingTextarea2">Message</label>
                 </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <br></br>
                 <button type="submit" value='Submit' className="btn btn-primary">Submit</button>
             </form>
