45d44e699364c4c6865a57967b6ff8aa438c6708
Elziekio <anthony.deamer@hotmail.com>
2021-10-31 18:44:16 +1000

    Filled out function for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import '../Styles/Footer.css';
 
 function Footer() {
 
     return (
-        <div>
-
+        <div className="icons">
+          <a href="https://github.com/Adeamer" className='anchor'>
+            <FaGithub size= '42'/>
+         </a>
+         <a href="https://www.linkedin.com/in/anthony-deamer-13b3b3175/" className='anchor'>
+            <FaLinkedin size= '42'/>
+         </a>
+         <a href="https://twitter.com/AnthonyDeamer" className='anchor'>
+            <FaTwitter size= '42'/>
+         </a>
         </div>
     )
 }
